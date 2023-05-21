f5c6d954b4a9c73bea10cec56e55773eb38a14b3
Diogo Dias <diogodiasgrilo@gmail.com>
2023-05-21 21:05:52 +0100

    Updated parsing header file May 21, 21:05

--- srcs/parser/parsing.h
+++ srcs/parser/parsing.h
@@ -4,6 +4,8 @@
 # include <stdlib.h>
 # include "../libft/libft.h"
 
+struct s_simple_cmds;
+
 typedef enum s_tokens
 {
 	PIPE = 1,
@@ -23,14 +25,31 @@ typedef struct s_lexer
 	struct s_lexer	*prev;
 }	t_lexer;
 
+typedef struct s_redirect_builder
+{
+    int     i1;
+    int     i2;
+    t_lexer *redirec;
+    t_lexer *returner;
+}               t_redirect_builder;
+
 typedef struct s_lexer_builder
 {
+    int     index;
     int     i;
     int     j;
     char    *str;
     t_lexer *start;
     t_lexer *node;
 }               t_lexer_builder;
+
+typedef struct s_cmds_builder
+{
+    int                     amount;
+    t_lexer                 *saver;
+    struct s_simple_cmds   *start;
+    struct s_simple_cmds   *node;
+}               t_cmds_builder;
 
 typedef struct s_simple_cmds
 {
@@ -42,5 +61,11 @@ typedef struct s_simple_cmds
 	struct s_simple_cmds	*next;
 	struct s_simple_cmds	*prev;
 }	t_simple_cmds;
+
+t_lexer         *lexer_lstlast(t_lexer *lst);
+void            ft_lexerdel_first(t_lexer **lst);
+t_lexer         *ft_lexerclear_one(t_lexer **lst);
+t_simple_cmds	*cmds_lstlast(t_simple_cmds *lst);
+void            ft_lexeradd_back(t_lexer **lst, t_lexer *new);
 
 #endif
