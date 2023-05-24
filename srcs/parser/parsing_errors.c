fa23a4fb775c0902390c584c50c252f26af8971d
Diogo Dias <diogodiasgrilo@gmail.com>
2023-05-24 17:11:56 +0100

    Updated parser error handling file

--- srcs/parser/parsing_errors.c
+++ srcs/parser/parsing_errors.c
@@ -3,33 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_errors.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: martiper <martiper@student.42.fr>          +#+  +:+       +#+        */
+/*   By: diogpere <diogpere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 12:03:16 by diogpere          #+#    #+#             */
-/*   Updated: 2023/05/24 13:43:14 by martiper         ###   ########.fr       */
+/*   Updated: 2023/05/24 17:10:57 by diogpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parser/parsing.h"
-
-t_parser_lexer	*parser_ft_lexerclear_one(t_parser_lexer **lst)
-{
-	if (!lst || !(*lst))
-		return (NULL);
-	if ((*lst)->str)
-	{
-		free((*lst)->str);
-		(*lst)->str = NULL;
-	}
-	free(*lst);
-	*lst = NULL;
-	return (NULL);
-}
+#include "../../include/parser/parsing.h"
 
 t_parser_simple_cmds	*parser_ft_simple_cmds_clear_one(\
 	t_parser_simple_cmds **lst)
 {
 	int				i;
+	t_parser_lexer	*redirections;
 
 	i = 0;
 	if ((*lst)->str)
@@ -43,7 +30,8 @@ t_parser_simple_cmds	*parser_ft_simple_cmds_clear_one(\
 		free((*lst)->str);
 		(*lst)->str = NULL;
 	}
-	parser_clear_lexer((*lst)->redirections);
+	redirections = (*lst)->redirections;
+	parser_ft_lexerclear_one(&redirections);
 	free(*lst);
 	*lst = NULL;
 	return (NULL);
@@ -75,7 +63,7 @@ int	parser_check_quotes(t_parser_lexer *start, t_parser_lexer *guide, char *str)
 int	parser_error_printer(t_parser_lexer *start, t_parser_lexer \
 	*guide, char *str, t_parser_simple_cmds *cmds)
 {
-	if (!str)
+	if (!str && guide)
 		printf("syntax error near unexpected token '%s'\n", guide->str);
 	else if (!str && !guide)
 		printf("memory allocation error\n");
@@ -86,14 +74,47 @@ int	parser_error_printer(t_parser_lexer *start, t_parser_lexer \
 	return (1);
 }
 
+void	check_tokens(t_parser_lexer *guide, int *innercount, int *count)
+{
+	int				i;
+	char			token;
+	t_parser_lexer	*saver;
+
+	i = 0;
+	token = guide->str[0];
+	while (guide->str[++i])
+	{
+		if (guide->str[i] != token)
+		{
+			*innercount = 0;
+			break ;
+		}
+		else
+			*innercount += 1;
+	}
+	saver = guide;
+	while (saver && saver->token != 0 && (saver->str[0] == guide->str[0] \
+		|| saver->token == guide->token))
+	{
+		*count += 1;
+		saver = saver->next;
+	}
+}
+
 int	parser_check_errors(t_parser_lexer *start, t_parser_lexer *guide, char *str)
 {
+	int				count;
+	int				innercount;
+
 	while (guide)
 	{
 		if (guide->token > 0 && (!guide->next || (guide->next && \
 			!guide->next->str[0])))
 			return (parser_error_printer(start, guide, 0, 0));
-		if (guide->token != 0 && (guide->token == guide->next->token))
+		count = 0;
+		innercount = 0;
+		check_tokens(guide, &innercount, &count);
+		if (count > 1 || innercount > 1)
 			return (parser_error_printer(start, guide, 0, 0));
 		guide = guide->next;
 	}
