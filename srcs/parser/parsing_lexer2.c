c2f4db4de6ef7381dee3746851265431351924e9
Diogo Dias <diogodiasgrilo@gmail.com>
2023-05-28 20:39:17 +0100

    Updated lexer2 for parsing of minishell

--- srcs/parser/parsing_lexer2.c
+++ srcs/parser/parsing_lexer2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_lexer2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: martiper <martiper@student.42.fr>          +#+  +:+       +#+        */
+/*   By: diogpere <diogpere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 22:11:59 by diogpere          #+#    #+#             */
-/*   Updated: 2023/05/28 18:28:16 by martiper         ###   ########.fr       */
+/*   Updated: 2023/05/26 14:19:22 by diogpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 void	parser_handle_side_quotes(t_parser_lexer_builder *b, char c)
 {
 	char	other_set;
-	char	set[3];
 
 	other_set = c;
 	if (c == '\'')
@@ -29,11 +28,10 @@ void	parser_handle_side_quotes(t_parser_lexer_builder *b, char c)
 			c = other_set;
 		if (b->str[b->j + 1] != c && b->str[b->j + 1] != other_set \
 			&& b->str[b->j])
-		{
-			ft_sprintf(set, 3, "%c%c", c, '\x04');
-			ft_strrep(&b->str, b->j, 1, set);
-		}
-		b->j += 2;
+			ft_strrep(&b->str, b->j, 1, "\x04");
+		else
+			ft_strrep(&b->str, b->j, 1, "");
+		b->j += 1;
 		while (b->str[b->j] && b->str[b->j] != c && b->str[b->j] != 32)
 			b->j++;
 	}
@@ -50,7 +48,7 @@ void	parser_handle_double_quotes(t_parser_lexer_builder *b)
 	if (b->j - b->i > 1 && (b->str[b->j] == '\"' || b->str[b->j] == '\'' \
 		|| b->str[b->j] == 32 || !b->str[b->j]))
 	{
-		b->node->str = ft_substr(b->str, b->i + 1, b->j - b->i);
+		b->node->str = ft_substr(b->str, b->i + 1, b->j - b->i - 1);
 		if (b->node->str[0])
 		{
 			tmp = b->node->str;
@@ -77,7 +75,7 @@ void	parser_handle_quotes(t_parser_lexer_builder *b)
 	if (b->j - b->i > 1 && (b->str[b->j] == '\'' || b->str[b->j] == '\"' \
 		|| b->str[b->j] == 32 || !b->str[b->j]))
 	{
-		b->node->str = ft_substr(b->str, b->i + 1, b->j - b->i);
+		b->node->str = ft_substr(b->str, b->i + 1, b->j - b->i - 1);
 		if (b->node->str[0])
 		{
 			tmp = b->node->str;
