12318243a29fa8b9fcf8d568c82e2f6383dfbfb4
MM1212 <mm.jogos.game@gmail.com>
2023-06-05 11:02:46 +0100

    fix(includes/signals): check if macro is defined

--- include/signals/signals.h
+++ include/signals/signals.h
@@ -6,15 +6,19 @@
 /*   By: martiper <martiper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 12:09:56 by martiper          #+#    #+#             */
-/*   Updated: 2023/06/05 10:56:21 by martiper         ###   ########.fr       */
+/*   Updated: 2023/06/05 11:02:31 by martiper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIGNALS_H
 # define SIGNALS_H
 
-# define _XOPEN_SOURCE 700
-# define _DEFAULT_SOURCE
+# ifndef _XOPEN_SOURCE
+#  define _XOPEN_SOURCE 700
+# endif
+# ifndef _DEFAULT_SOURCE
+#  define _DEFAULT_SOURCE
+# endif
 # include <shared.h>
 
 void	signals_hook(void);
